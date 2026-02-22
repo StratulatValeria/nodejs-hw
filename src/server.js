4e7bdbe8aa26b9bc3fa4ec1ba9508178bbd9d44d
Tenebris An <y2222672@gmail.com>
2026-02-22 15:33:59 +0200

    completed homework 1: express server setup

--- src/server.js
+++ src/server.js
@@ -1,8 +1,29 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
+import pino from 'pino-http';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
@@ -13,6 +34,21 @@ app.get('/notes', (req, res) => {
 app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+// тестимо помилку для перевірки обробки помилок
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: err.message,
+  });
 });
 
 app.listen(PORT, () => {

5760eaee7cc420dcf8812a36576a304f86d52eea
Tenebris An <y2222672@gmail.com>
2026-03-15 18:08:59 +0200

    add middleware auth

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -13,12 +13,15 @@ import {
   deleteNote,
   updateNote,
 } from '../controllers/notesController.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
 
+router.use(authenticate);
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 router.post('/notes', celebrate(createNoteSchema), createNote);
 router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
 router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
+
 export default router;
